f4addfe233b77c2832b73b73932428e7155850a2
GUNJack1994 <lukasz.barczykowski94@gmail.com>
2024-01-15 21:51:00 +0100

    Test commit

---
+++ CanvasReplyTestTask/Hooks/CalculatorHooks.cs
@@ -0,0 +1,23 @@
+﻿using CalculatorSelenium.Specs.PageObjects;
+using CanvasReplyTestTask.Drivers;
+using TechTalk.SpecFlow;
+
+namespace CalculatorSelenium.Specs.Hooks
+{
+    /// <summary>
+    /// Calculator related hooks
+    /// </summary>
+    [Binding]
+    public class CalculatorHooks
+    {
+        ///<summary>
+        ///  Reset the calculator before each scenario tagged with "Calculator"
+        /// </summary>
+        [BeforeScenario("Calculator")]
+        public static void BeforeScenario(Driver driver)
+        {
+            var calculatorPageObject = new CalculatorPageObject(driver.Current);
+            calculatorPageObject.EnsureCalculatorIsOpenAndReset();
+        }
+    }
+}
---
+++ CanvasReplyTestTask/Hooks/SharedBrowserHooks.cs
@@ -0,0 +1,16 @@
+﻿using BoDi;
+using CanvasReplyTestTask.Drivers;
+
+namespace CanvasReplyTestTask.Hooks
+{
+    [Binding]
+    public class SharedBrowserHooks
+    {
+        [BeforeTestRun]
+        public static void BeforeTestRun(ObjectContainer testThreadContainer)
+        {
+            //Initialize a shared BrowserDriver in the global container
+            testThreadContainer.BaseContainer.Resolve<Driver>();
+        }
+    }
+}
